fd4a2c70ea3fc22b54426b28cf7b5b8dd8b6873a
sibelcaglayan0919 <sibelcaglayan0919@gmail.com>
2026-09-16 20:18:12 +0300

    İlan güncellendi: didim-akbukte-satilik-villa

--- assets/js/properties.js
+++ assets/js/properties.js
@@ -156,7 +156,7 @@ const PROPERTIES = [
       tr: "Akbukte mustakil Villa 4+1 havuzlu",
       en: "From akbuk 4 bedrom swimming pool"
     },
-    price: 34.5,
+    price: 3500000,
     currency: "TRY",
     status: "sale",  // sale | rent | sold
     type: "villa",  // villa | daire | arsa | isyeri
